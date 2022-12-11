35527870527cf7b32e912cdecc42283f6846e9a9
Dimaq1 <levadni1717@gmail.com>
2022-12-11 03:50:26 +0200

    оформление секций experience + slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,36 @@
+
 $(document).ready(function () {
-  $('.menu__btn').on("click", function () {
-    $(this).toggleClass('active');
+  $(".menu__btn").on("click", function () {
+    $(this).toggleClass("-active");
+    $(".menu__list").stop(true, true).slideToggle(300);
   });
 });
+
+// считали DOM элементы
+const headersList = document.querySelectorAll(".slider__header");
+const notesList = document.querySelectorAll(".slider__note");
+const descriptionsList = document.querySelectorAll(".slider__description");
+const indicatorsList = document.querySelectorAll(".slider__indicator");
+
+let index = 0;
+let interval = 4000;
+let heightsArr = [];
+let heightMax = null;
+
+setInterval(() => {
+  // снимаем классы active для первых элементов
+  headersList[index].classList.toggle("-active");
+  notesList[index].classList.toggle("-active");
+  indicatorsList[index].classList.toggle("-active");
+  // увеличиваем индекс, пока не превышено количество элементов
+  index = (index + 1) % headersList.length;
+  // ставим классы active следующим элементам
+  headersList[index].classList.toggle("-active");
+  notesList[index].classList.toggle("-active");
+  indicatorsList[index].classList.toggle("-active");
+}, interval);
+
+// вычисление и изменение высоты блока описания под максимальный текст
+descriptionsList.forEach(el => heightsArr.push(el.clientHeight));
+heightMax = Math.max(...heightsArr);
+descriptionsList.forEach(el => el.style.height = `${heightMax}px`);
