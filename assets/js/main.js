e8d705a03e63bc0866340b3ed4722ca98bf6750f
Dimaq1 <levadni1717@gmail.com>
2023-01-19 17:29:16 +0200

    finish page publications

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,37 @@ setInterval(() => {
 }, interval);
 
 // вычисление и изменение высоты блока описания под максимальный текст
-// descriptionsList.forEach(el => heightsArr.push(el.clientHeight));
-// heightMax = Math.max(...heightsArr);
-// descriptionsList.forEach(el => el.style.height = `${heightMax}px`);
+descriptionsList.forEach(el => heightsArr.push(el.clientHeight));
+heightMax = Math.max(...heightsArr);
+descriptionsList.forEach(el => el.style.height = `${heightMax}px`);
+
+'use strict';
+
+const switchButton = document.querySelector('.page-publications__switcher');
+const switchBtnRight = document.querySelector('.page-publications__switcher-button.right');
+const switchBtnLeft = document.querySelector('.page-publications__switcher-button.left');
+const activeSwitch = document.querySelector('.page-publications__switcher-active');
+
+function switchLeft() {
+  switchBtnRight.classList.remove('active-case');
+  switchBtnLeft.classList.add('active-case');
+  activeSwitch.style.left = '2px';
+}
+
+function switchRight() {
+  switchBtnRight.classList.add('active-case');
+  switchBtnLeft.classList.remove('active-case');
+  activeSwitch.style.left = '50%';
+}
+
+switchBtnLeft.addEventListener('click', function () {
+  switchLeft();
+}, false);
+
+switchBtnRight.addEventListener('click', function () {
+  switchRight();
+}, false);
+
+
+
+
