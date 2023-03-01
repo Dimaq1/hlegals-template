7c7c37cc213c5c7e691e602dbc3cb87ee5c49307
Dimaq1 <levadni1717@gmail.com>
2023-03-01 11:29:51 +0200

    css + js

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -6,3 +6,55 @@ function scrollDown() {
 }
 
 btnClickScroll.addEventListener('click', scrollDown);
+
+
+
+
+
+
+
+
+let sliderHeader = document.querySelectorAll('.slider__header');
+let sliderNote = document.querySelectorAll('.slider__note');
+
+console.log(sliderHeader);
+
+
+setInterval(() => {
+  sliderHeader.forEach((el, index, arr) => {
+    return arr[index].classList.toggle('-active')
+  })
+}, 2000)
+// setInterval(() => {
+//   for (let i = 0; i < sliderHeader.length; i++) {
+//     sliderHeader[i].classList.toggle('-active')
+//   }
+// }, 4000)
+
+
+
+
+
+// sliderHeader.forEach((el, index, arr) => {
+//   arr[index].addEventListener('click', function () {
+//     el.classList.toggle('-active')
+
+//   })
+// })
+
+
+
+// sliderNote.forEach((el, index, arr) => {
+//   sliderHeader[index].addEventListener('click', function () {
+//     el.classList.toggle('-active')
+//   })
+// })
+
+// for (let i = 0; i < sliderHeader.length; i++) {
+//   if (sliderHeader[i].classList.add('-active')) {
+//     // sliderHeader[i++] = null;
+//     sliderHeader[i++].addEventListener('click', function () {
+//       sliderHeader[i].classList.add('')
+//     })
+//   }
+// }
